8f81353341edbba20db91206aeaafa132926b015
matvalcke <mvalcke@adin.pro>
2024-07-03 15:04:50 +0200

    Modify component

--- artAdminPanelApp/src/app/app-routing.module.ts
+++ artAdminPanelApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
